0a750f372be5550271413af8bd1466c0077a5ef1
Shah <Shah@DESKTOP-THOM38U>
2022-09-27 10:35:25 +0300

    Добавьте файлы проекта.

---
+++ Ex.43/Lord.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._43
+{
+    internal class Lord
+    {
+        public static void DeleteByIndex<T>(ref T[] array, int index)
+        {
+            if (array.Length > index && index >= 0)
+            {
+                T[] newArray = new T[array.Length - 1];
+
+                for (int i = 0, j = 0; i < newArray.Length; i++, j++)
+                {
+                    if (j == index) i--;
+                    else newArray[i] = array[j];
+                }
+
+                array = newArray;
+            }
+        }
+        public static void DeleteLastElement<T>(ref T[] array)
+        {
+            T[] newArray = new T[array.Length - 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+        public static void DeleteFirstElement<T>(ref T[] array)
+        {
+            T[] newArray = new T[array.Length - 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = array[i + 1];
+            }
+
+            array = newArray;
+        }
+        public static void AddByIndex<T>(ref T[] array, int index, T value)
+        {
+            T[] newArray = new T[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index) newArray[i] = value;
+                else newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+        public static void AddToEnd<T>(ref T[] array, T value)
+        {
+            T[] newArray = new T[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            newArray[array.Length] = value;
+
+            array = newArray;
+        }
+        public static void AddToTop<T> (ref T[] array, T value)
+        {
+            T[] newArray = new T[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i + 1] = array[i];
+            }
+
+            newArray[0] = value;
+
+            array = newArray;
+        }
+        public static void Resize<T>(ref T[] array, int counter)
+        {
+            T[] newArray = new T[counter];
+
+            for (int i = 0; i < newArray.Length && i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+        public static void WriteArray<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Ex.43/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._43
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] array = {0, 1, 2, 3, 4, 5};
+            Lord.WriteArray(array);
+
+            Lord.Resize(ref array, 4);
+            Lord.WriteArray(array);
+
+            Lord.AddToTop(ref array, 4);
+            Lord.WriteArray(array);
+
+            Lord.AddToEnd(ref array, 5);
+            Lord.WriteArray(array);
+
+            Lord.AddByIndex(ref array, 5, 4);
+            Lord.WriteArray(array);
+
+            Lord.DeleteFirstElement(ref array);
+            Lord.WriteArray(array);
+
+            Lord.DeleteLastElement(ref array);
+            Lord.WriteArray(array);
+
+            Lord.DeleteByIndex(ref array, 3);
+            Lord.WriteArray(array);
+        }
+    }
+}
